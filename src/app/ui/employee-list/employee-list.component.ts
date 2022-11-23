274068e5c6b71df5738a027a8f3a0129c13d4c46
Marcin Ceglarski <marcin.ceglarski0@gmail.com>
2022-11-23 19:07:45 +0100

    feat: exercise 1, added employee data to the table, added styles for employee-list component

--- src/app/ui/employee-list/employee-list.component.ts
+++ src/app/ui/employee-list/employee-list.component.ts
@@ -3,15 +3,19 @@ import {
   Component,
   ViewEncapsulation,
 } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { EmployeeModel } from '../../model/employee.model';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'employee-list',
   templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.scss'],
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EmployeeListComponent {
-  data$: Observable<EmployeeModel[] | null> = of(null);
+  constructor(private _client: HttpClient) {  }
+
+  public data$: Observable<EmployeeModel[] | null> = this._client.get<EmployeeModel[]>('/assets/data/employees.json');
 }
