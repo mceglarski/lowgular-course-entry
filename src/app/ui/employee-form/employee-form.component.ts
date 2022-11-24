44a9fec34fab4444f7f98748e74bd1eb4a7f49e0
Marcin Ceglarski <marcin.ceglarski0@gmail.com>
2022-11-24 20:13:15 +0100

    feat: FB2 (CRUD) ex.2, added alert on submit

--- src/app/ui/employee-form/employee-form.component.ts
+++ src/app/ui/employee-form/employee-form.component.ts
@@ -12,4 +12,12 @@ export class EmployeeFormComponent {
     age: new FormControl(null, [Validators.min(0)]),
     salary: new FormControl(null, [Validators.required, Validators.min(0)]),
   });
+
+  public onSubmit(form: { name: string; age: number; salary: number }): void {
+    alert(
+      'User was successfully added to the database. Name: ' + form.name +
+        ', age: ' + form.age +
+        ', salary: ' + form.salary
+    );
+  }
 }
