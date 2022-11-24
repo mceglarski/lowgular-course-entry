0a751367f07d18700c6ab4ee36a335f11eb017f0
Marcin Ceglarski <marcin.ceglarski0@gmail.com>
2022-11-24 22:32:54 +0100

    feat: FB2 (CRUD) ex.5, map getAll() response to the new model

--- src/app/model/employee.model.ts
+++ src/app/model/employee.model.ts
@@ -4,3 +4,12 @@ export interface EmployeeModel {
   email: string;
   image: string;
 }
+
+export interface EmployeeResponse {
+  status: string;
+  id: string;
+  employee_name: string;
+  employee_salary: string;
+  employee_age: string;
+  profile_image: string;
+}
--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,22 +1,44 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import {PersonModel} from "../model/person.model";
-import {CreateEmployeeModel} from "../model/create-employee.model";
+import { map, Observable } from 'rxjs';
+import { PersonModel } from '../model/person.model';
+import { CreateEmployeeModel } from '../model/create-employee.model';
+import { ApiResponse } from './api.response';
+import { EmployeeResponse } from '../model/employee.model';
 
 @Injectable()
 export class EmployeeService {
   constructor(private _client: HttpClient) {}
 
   public getAll(): Observable<PersonModel[]> {
-    return this._client.get<PersonModel[]>('/assets/data/people.json');
+    return this._client
+      .get<ApiResponse<EmployeeResponse[]>>(
+        'https://dummy.restapiexample.com/api/v1/employees'
+      )
+      .pipe(
+        map((response: ApiResponse<EmployeeResponse[]>) => {
+          return response.data.map((employeeResponse: EmployeeResponse) => {
+            return {
+              name: employeeResponse.employee_name,
+              personalNumber: employeeResponse.id,
+              img: employeeResponse.profile_image,
+              mail: (employeeResponse.employee_name).replace(/\s/g, "") + '@lowgular.io',
+            };
+          });
+        })
+      );
   }
 
   public create(employee: CreateEmployeeModel): Observable<any> {
-    return this._client.post('https://dummy.restapiexample.com/api/v1/create', employee);
+    return this._client.post(
+      'https://dummy.restapiexample.com/api/v1/create',
+      employee
+    );
   }
 
   public delete(id: string): Observable<any> {
-    return this._client.delete('https://dummy.restapiexample.com/api/v1/delete/' + id);
+    return this._client.delete(
+      'https://dummy.restapiexample.com/api/v1/delete/' + id
+    );
   }
 }
